7484ab967bfece2c35b10f2db5d02a06b9351967
BaiGuangMei <2868653801@qq.com>
2024-08-26 17:23:38 +0800

    first commit

---
+++ components/theme-item/theme-item.vue
@@ -0,0 +1,77 @@
+<template>
+	<view class="themeitem">
+		<navigator url="" class="box" v-if="!isMore">
+			<image class="pic" src="../../common/imgs/classify1.jpg" mode="aspectFill"></image>
+			<view class="mask">明星美女</view>
+			<view class="tab">3天前更新</view>
+		</navigator>
+		
+		<navigator url="" class="box more" v-else>
+			<image class="pic" src="../../common/imgs/more.jpg" mode="aspectFill"></image>
+			<view class="mask">
+				<uni-icons type="more-filled" size="34" color="#fff"></uni-icons>
+				<view class="text">更多</view>
+			</view>
+		</navigator>
+	</view>
+</template>
+
+<script setup>
+defineProps({
+	isMore:{
+		type:Boolean,
+		default:false
+	}
+})
+</script>
+
+<style lang="scss" scoped>
+	.themeitem{
+		.box{
+			height:340rpx;
+			border-radius: 10rpx;
+			overflow: hidden;
+			position: relative;
+			.pic{
+				width: 100%;
+				height: 100%;
+			}
+			.mask{
+				width: 100%;
+				height: 70rpx;
+				position: absolute;
+				bottom: 0;
+				left: 0;
+				background: rgba(0,0,0,0.2);
+				display: flex;
+				align-items: center;
+				justify-content: center;
+				backdrop-filter: blur(20rpx);
+				font-size: 30rpx;
+				font-weight: 600;
+				color: #fff;
+			}
+			.tab{
+				position: absolute;
+				left: 0;
+				top: 0;
+				background: rgba(250,129,90,0.7);
+				backdrop-filter: blur(20rpx);
+				color: #fff;
+				font-size: 22rpx;
+				padding: 6rpx 14rpx;
+				border-radius: 0 0 20rpx 0;
+			}
+		}
+		.box.more{
+			.mask{
+				width: 100%;
+				height: 100%;
+				flex-direction: column;
+			}
+			.text{
+				font-size: 28rpx;
+			}
+		}
+	}
+</style>
